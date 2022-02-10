203b0a6c633c0ff999eca87f060152200a1d76bd
Zusan <sudharsan58@gmail.com>
2022-02-10 16:11:15 +0530

    First commit

--- BlinkTest.ino
+++ BlinkTest.ino
@@ -24,7 +24,7 @@
 
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
+  // initialize digital pin LED_BUILTIN as an output.get
   pinMode(1, OUTPUT);
 }
 
